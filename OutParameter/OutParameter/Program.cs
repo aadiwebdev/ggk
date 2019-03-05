473d4f9a665ea855ce7aa2a0376895bfa1fa5c79
Aditya Tangirala <Aditya.Tangirala@ggktech.local>
2019-03-05 12:18:50 +0530

    add OutParameter Project

---
+++ OutParameter/OutParameter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace OutParameter
+{
+    public class Program
+    {
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        /// <param></param>
+          
+        public static void Main()
+        {
+            int number = 101;
+            Console.WriteLine("\n number value in main:" + number);
+            Thread thread = new Thread(() => OutMethod(out number));
+            thread.Start();
+            //main thread sleep for 50ms
+            Thread.Sleep(50);
+            Console.WriteLine("\n In OutMethod after init:" + number);
+            Console.ReadLine();
+        }
+        
+        /// <summary>
+        /// method containing an out Parameter
+        /// </summary>
+        /// <param name="temp">out parameter</param>
+        public static void OutMethod(out int temp)
+        {
+            Console.WriteLine("Entered OutMethod....");
+            //OutMethod thread sleep for 100ms 
+            Thread.Sleep(100);
+            temp = 62;
+        }
+    }
+}
